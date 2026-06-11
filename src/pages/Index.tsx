a0229136d2aebede4cba5dd121de4b0278355d1b
TenzingJangsel <142163224+TenzingJangsel@users.noreply.github.com>
2026-06-11 11:32:59 +0600

    Add files via upload

---
+++ src/pages/Index.tsx
@@ -0,0 +1,20 @@
+const Index = () => {
+  return (
+    <iframe
+      src="/lumini.html"
+      title="Lumini Social"
+      style={{
+        position: "fixed",
+        top: 0,
+        left: 0,
+        width: "100vw",
+        height: "100vh",
+        border: "none",
+        margin: 0,
+        padding: 0,
+      }}
+    />
+  );
+};
+
+export default Index;
