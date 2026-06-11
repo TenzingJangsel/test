6f740097d832ef3f82ec2ed68f5b95f45d400a01
TenzingJangsel <142163224+TenzingJangsel@users.noreply.github.com>
2026-06-11 11:10:31 +0600

    Delete src directory

--- src/pages/Index.tsx
+++
@@ -1,8 +0,0 @@
-import Feed from "./Feed";
-
-/**
- * Index route. Previously this was a full-screen iframe wrapping
- * lumini.html. Now it renders the real React Feed inside the Layout.
- */
-const Index = () => <Feed />;
-export default Index;
